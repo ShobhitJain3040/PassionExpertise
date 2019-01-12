c2fc27f196240408cbe282d792d24c4da1faea5a
shalu tyagi <shalutyagi2811@gmail.com>
2019-01-12 19:41:57 +0530

    add baseView Controller

---
+++ PassionExperties/ViewController/Dreamer/DreamerNotificationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DreamerNotificationViewController.swift
+//  PassionExperties
+//
+//  Created by shalu tyagi on 12/01/19.
+//  Copyright © 2019 shalu tyagi. All rights reserved.
+//
+
+import UIKit
+
+class DreamerNotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Notifications"
+    self.tableView.estimatedRowHeight = 400
+    self.tableView.tableFooterView = UIView()
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell", for: indexPath) as! NotificationCell
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 137
+  }
+}
