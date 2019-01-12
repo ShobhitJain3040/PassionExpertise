8f9a286f33886aebe91b95ea934a81521da49db9
Shobhit Jain <shobhit.jain@aspiringminds.in>
2019-01-13 03:13:13 +0530

    random change

--- PassionExperties/ViewController/Dreamer/DreamerChatViewController.swift
+++ PassionExperties/ViewController/Dreamer/DreamerChatViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class DreamerChatViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class DreamerChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
   
+  var tableView: UITableView?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView = UITableView()
+    self.view.addSubview(tableView!)
+    
+    tableView?.dataSource = self
+    tableView?.delegate = self
+    // Do any additional setup after loading the view.
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
 }
