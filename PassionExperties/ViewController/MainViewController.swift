9328c1c8a6fae8fe91f8b5a9a17df79458c169de
Shobhit Jain <shobhit.jain@aspiringminds.in>
2019-01-12 19:45:40 +0530

    Worked on sign up

--- PassionExperties/ViewController/MainViewController.swift
+++ PassionExperties/ViewController/MainViewController.swift
@@ -15,6 +15,16 @@ class MainViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
+  
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.navigationController?.setNavigationBarHidden(true, animated: false)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    self.navigationController?.setNavigationBarHidden(false, animated: false)
+  }
 
 }
