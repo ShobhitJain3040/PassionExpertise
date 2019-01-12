97c1b03ef67a660241ecbe604bdfccae7b22df83
shalu tyagi <shalutyagi2811@gmail.com>
2019-01-12 14:34:48 +0530

    add setup

---
+++ PassionExperties-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PassionExperties.h
+//  PassionExperties
+//
+//  Created by shalu tyagi on 12/01/19.
+//  Copyright © 2019 shalu tyagi. All rights reserved.
+//
+
+#import <CommonCrypto/CommonCrypto.h>
+#import "NMRangeSlider.h"
