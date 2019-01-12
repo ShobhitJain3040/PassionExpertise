1b8fef07ec1488eba182ed66f823748166865042
shalu tyagi <shalutyagi2811@gmail.com>
2019-01-12 14:36:30 +0530

    fix error

--- PassionExperties/AppDelegate/AppDelegate.swift
+++ PassionExperties/AppDelegate/AppDelegate.swift
@@ -45,48 +45,72 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   // MARK: - Core Data stack
-
-  lazy var persistentContainer: NSPersistentContainer = {
-      /*
-       The persistent container for the application. This implementation
-       creates and returns a container, having loaded the store for the
-       application to it. This property is optional since there are legitimate
-       error conditions that could cause the creation of the store to fail.
-      */
-      let container = NSPersistentContainer(name: "PassionExperties")
-      container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-          if let error = error as NSError? {
-              // Replace this implementation with code to handle the error appropriately.
-              // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-               
-              /*
-               Typical reasons for an error here include:
-               * The parent directory does not exist, cannot be created, or disallows writing.
-               * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-               * The device is out of space.
-               * The store could not be migrated to the current model version.
-               Check the error message to determine what the actual problem was.
-               */
-              fatalError("Unresolved error \(error), \(error.userInfo)")
-          }
-      })
-      return container
+  
+  lazy var applicationDocumentsDirectory: URL = {
+    // The directory the application uses to store the Core Data store file. This code uses a directory named "com.am.myamcat.Myamcat" in the application's documents Application Support directory.
+    let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return urls[urls.count-1]
   }()
-
-  // MARK: - Core Data Saving support
-
-  func saveContext () {
-      let context = persistentContainer.viewContext
-      if context.hasChanges {
-          do {
-              try context.save()
-          } catch {
-              // Replace this implementation with code to handle the error appropriately.
-              // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-              let nserror = error as NSError
-              fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-          }
+  
+  lazy var managedObjectModel: NSManagedObjectModel = {
+    // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+    let modelURL = Bundle.main.url(forResource: "Demo1", withExtension: "momd")!
+    return NSManagedObjectModel(contentsOf: modelURL)!
+  }()
+  
+  
+  lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+    // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+    // Create the coordinator and store
+    let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+    let url = self.applicationDocumentsDirectory.appendingPathComponent("Demo1.sqlite")
+    do {
+      try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+    }
+    catch {
+      debugLog("There was an error creating or loading the application's saved data.")
+      debugLog("Sqlite file is:\(url)")
+      do {
+        //Ideally we should have done the migration but, didn't take care of that. For now we're just deleting the db file
+        //and will try to again initialize the persistence store coordinator.
+        try FileManager.default.removeItem(at: url)
+        //Now the old model DB is deleted we'll try once again to configure persistence store coordinator
+        try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
       }
+      catch {
+        debugLog("Couldn't remove or add PSC")
+        //show AlertView here
+        let closeAction = UIAlertAction(title: "EXIT", style: .default) { (action) in
+          //close the app
+          abort()
+        }
+        Utilities.showAlertForMessage(message: "DELETE THE APP", vc: (self.window?.rootViewController)!)
+      }
+    }
+    
+    return coordinator
+  }()
+  
+  lazy var managedObjectContext: NSManagedObjectContext = {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+    let coordinator = self.persistentStoreCoordinator
+    var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    managedObjectContext.persistentStoreCoordinator = coordinator
+    return managedObjectContext
+  }()
+  
+  // MARK: - Core Data Saving support
+  func saveContext () {
+    if managedObjectContext.hasChanges {
+      do {
+        try managedObjectContext.save()
+      } catch {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        let nserror = error as NSError
+        debugLog("Unresolved error \(nserror) : \(nserror.userInfo)")
+      }
+    }
   }
 
 }
